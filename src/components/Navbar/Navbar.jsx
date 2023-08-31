ec048ecbf80d25c4c627373f514ac31b56d9fa8e
Muslimov-Xasan <muslimovxasan050@gmail.com>
2023-08-31 16:53:45 +0500

    navbar change nav link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,30 @@
 import "./Navbar.css";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <>
-    <div className="contianer">
+      <div className="contianer">
         <nav className="nav">
-            <ul className="nav-list">
-                <li className="nav-item">Behavior</li>
-                <li className="nav-item">Appearance</li>
-                <li className="nav-item">Custom Fields</li>
-                <li className="nav-item">Questions</li>
-                <li className="nav-item">URL Control</li>
-            </ul>
+          <ul className="nav-list">
+            <li className="nav-item">
+              <Link className="nav-link" to={"/home"}>Behavior</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={"#/home"}>Appearance</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={"/home"}>Custom Fields</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={"/home"}>Questions</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={"/home"}>URL Control</Link>
+            </li>
+          </ul>
         </nav>
-    </div>
+      </div>
     </>
   );
 };
